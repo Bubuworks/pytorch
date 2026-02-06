6ec0fa799cf056857fddb13fcec2c7c18d19d654
Bubuworks <bubupersonal@tutamail.com>
2026-02-06 17:21:53 +0000

    General improvements
    Replaced the old system where the filename was hardcoded to a system where the filename gets extracted from the URL automatically with %%~nxF.
    
    Fixed curl quoting by wrapping URL in quotes.
    
    Added error handling.
    
    Replaced del with del /f /q.

--- .circleci/scripts/driver_update.bat
+++ .circleci/scripts/driver_update.bat
@@ -1,8 +1,13 @@
+setlocal enabledelayedexpansion
 set "DRIVER_DOWNLOAD_LINK=https://s3.amazonaws.com/ossci-windows/452.39-data-center-tesla-desktop-win10-64bit-international.exe"
-curl --retry 3 --retry-all-errors -kL %DRIVER_DOWNLOAD_LINK% --output 452.39-data-center-tesla-desktop-win10-64bit-international.exe
-if errorlevel 1 exit /b 1
-
-start /wait 452.39-data-center-tesla-desktop-win10-64bit-international.exe -s -noreboot
-if errorlevel 1 exit /b 1
-
-del 452.39-data-center-tesla-desktop-win10-64bit-international.exe || ver > NUL
+for %%F in ("%DRIVER_DOWNLOAD_LINK%") do set "DRIVER_FILE=%%~nxF"
+curl --retry 3 --retry-all-errors -L "%DRIVER_DOWNLOAD_LINK%" --output "%DRIVER_FILE%" || (
+    echo Error: failed to download driver.
+    exit /b 1
+)
+start /wait "" "%DRIVER_FILE%" -s -noreboot || (
+    echo Error: driver installation failed.
+    exit /b 1
+)
+del /f /q "%DRIVER_FILE%" || echo Warning: failed to delete installer.
+endlocal
