0e883a74f896b51968651bc6dd5fe497534c4e26
Bubuworks <bubupersonal@tutamail.com>
2026-02-06 18:46:01 +0000

    Maintenance
    Added explicit checks for PACKAGE_NAME and DESIRED_CUDA,
    
    Quoted all paths.
    
    Added --retry 3 --retry-all-errors, skip if file exists.
    
    Applied patches in a loop with echo of which patch is being applied.
    
    Made Pushd and Popd redirect output to /dev/null.
    
    Replaced inline environment vars with by exporting INSTALL_DIR.
    
    Added debug.

--- .ci/magma/build_magma.sh
+++ .ci/magma/build_magma.sh
@@ -1,52 +1,58 @@
 #!/usr/bin/env bash
 
-set -eou pipefail
+set -eu -o pipefail
+[[ "${DEBUG:-}" == "1" ]] && set -x
 
-# Environment variables
-# The script expects DESIRED_CUDA and PACKAGE_NAME to be set
+: "${PACKAGE_NAME:?PACKAGE_NAME must be set}"
+: "${DESIRED_CUDA:?DESIRED_CUDA must be set}"
+
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 MAGMA_VERSION=2.6.1
 
-# Folders for the build
-PACKAGE_FILES=${ROOT_DIR}/magma/package_files # source patches and metadata
-PACKAGE_DIR=${ROOT_DIR}/magma/${PACKAGE_NAME} # build workspace
-PACKAGE_OUTPUT=${ROOT_DIR}/magma/output # where tarballs are stored
-PACKAGE_BUILD=${PACKAGE_DIR}/build # where the content of the tarball is prepared
-PACKAGE_RECIPE=${PACKAGE_BUILD}/info/recipe
-PACKAGE_LICENSE=${PACKAGE_BUILD}/info/licenses
-mkdir -p ${PACKAGE_DIR} ${PACKAGE_OUTPUT}/linux-64 ${PACKAGE_BUILD} ${PACKAGE_RECIPE} ${PACKAGE_LICENSE}
+PACKAGE_FILES="${ROOT_DIR}/magma/package_files"
+PACKAGE_DIR="${ROOT_DIR}/magma/${PACKAGE_NAME}"
+PACKAGE_OUTPUT="${ROOT_DIR}/magma/output"
+PACKAGE_BUILD="${PACKAGE_DIR}/build"
+PACKAGE_RECIPE="${PACKAGE_BUILD}/info/recipe"
+PACKAGE_LICENSE="${PACKAGE_BUILD}/info/licenses"
 
-# Fetch magma sources and verify checksum
-pushd ${PACKAGE_DIR}
-curl -LO http://icl.utk.edu/projectsfiles/magma/downloads/magma-${MAGMA_VERSION}.tar.gz
-tar zxf magma-${MAGMA_VERSION}.tar.gz
-sha256sum --check < ${PACKAGE_FILES}/magma-${MAGMA_VERSION}.sha256
-popd
+mkdir -p "${PACKAGE_DIR}" "${PACKAGE_OUTPUT}/linux-64" "${PACKAGE_BUILD}" "${PACKAGE_RECIPE}" "${PACKAGE_LICENSE}"
 
-# Apply patches and build
-pushd ${PACKAGE_DIR}/magma-${MAGMA_VERSION}
-patch < ${PACKAGE_FILES}/CMake.patch
-patch < ${PACKAGE_FILES}/cmakelists.patch
-patch -p0 < ${PACKAGE_FILES}/thread_queue.patch
-patch -p1 < ${PACKAGE_FILES}/cuda13.patch
-patch -p1 < ${PACKAGE_FILES}/getrf_shfl.patch
-patch -p1 < ${PACKAGE_FILES}/getrf_nbparam.patch
-# The build.sh script expects to be executed from the sources root folder
-INSTALL_DIR=${PACKAGE_BUILD} ${PACKAGE_FILES}/build.sh
-popd
+pushd "${PACKAGE_DIR}" >/dev/null
+MAGMA_TARBALL="magma-${MAGMA_VERSION}.tar.gz"
 
-# Package recipe, license and tarball
-# Folder and package name are backward compatible for the build workflow
-cp ${PACKAGE_FILES}/build.sh ${PACKAGE_RECIPE}/build.sh
-cp ${PACKAGE_FILES}/cuda13.patch ${PACKAGE_RECIPE}/cuda13.patch
-cp ${PACKAGE_FILES}/thread_queue.patch ${PACKAGE_RECIPE}/thread_queue.patch
-cp ${PACKAGE_FILES}/cmakelists.patch ${PACKAGE_RECIPE}/cmakelists.patch
-cp ${PACKAGE_FILES}/getrf_shfl.patch ${PACKAGE_RECIPE}/getrf_shfl.patch
-cp ${PACKAGE_FILES}/getrf_nbparam.patch ${PACKAGE_RECIPE}/getrf_nbparam.patch
-cp ${PACKAGE_FILES}/CMake.patch ${PACKAGE_RECIPE}/CMake.patch
-cp ${PACKAGE_FILES}/magma-${MAGMA_VERSION}.sha256 ${PACKAGE_RECIPE}/magma-${MAGMA_VERSION}.sha256
-cp ${PACKAGE_DIR}/magma-${MAGMA_VERSION}/COPYRIGHT ${PACKAGE_LICENSE}/COPYRIGHT
-pushd ${PACKAGE_BUILD}
-tar cjf ${PACKAGE_OUTPUT}/linux-64/${PACKAGE_NAME}-${MAGMA_VERSION}-1.tar.bz2 include lib info
-echo Built in ${PACKAGE_OUTPUT}/linux-64/${PACKAGE_NAME}-${MAGMA_VERSION}-1.tar.bz2
-popd
+if [[ ! -f "${MAGMA_TARBALL}" ]]; then
+    echo "Downloading MAGMA ${MAGMA_VERSION}..."
+    curl -LO --retry 3 --retry-all-errors "http://icl.utk.edu/projectsfiles/magma/downloads/${MAGMA_TARBALL}"
+fi
+
+tar zxf "${MAGMA_TARBALL}"
+sha256sum --check < "${PACKAGE_FILES}/magma-${MAGMA_VERSION}.sha256"
+popd >/dev/null
+
+pushd "${PACKAGE_DIR}/magma-${MAGMA_VERSION}" >/dev/null
+for patchfile in CMake.patch cmakelists.patch thread_queue.patch cuda13.patch getrf_shfl.patch getrf_nbparam.patch; do
+    patch_path="${PACKAGE_FILES}/${patchfile}"
+    echo "Applying patch: ${patch_path}"
+    if [[ "$patchfile" == "CMake.patch" || "$patchfile" == "cmakelists.patch" ]]; then
+        patch < "${patch_path}"
+    elif [[ "$patchfile" == "thread_queue.patch" ]]; then
+        patch -p0 < "${patch_path}"
+    else
+        patch -p1 < "${patch_path}"
+    fi
+done
+
+export INSTALL_DIR="${PACKAGE_BUILD}"
+"${PACKAGE_FILES}/build.sh"
+popd >/dev/null
+
+for file in build.sh cuda13.patch thread_queue.patch cmakelists.patch getrf_shfl.patch getrf_nbparam.patch CMake.patch magma-${MAGMA_VERSION}.sha256; do
+    cp "${PACKAGE_FILES}/${file}" "${PACKAGE_RECIPE}/"
+done
+cp "${PACKAGE_DIR}/magma-${MAGMA_VERSION}/COPYRIGHT" "${PACKAGE_LICENSE}/COPYRIGHT"
+
+pushd "${PACKAGE_BUILD}" >/dev/null
+tar cjf "${PACKAGE_OUTPUT}/linux-64/${PACKAGE_NAME}-${MAGMA_VERSION}-1.tar.bz2" include lib info
+echo "Built package at ${PACKAGE_OUTPUT}/linux-64/${PACKAGE_NAME}-${MAGMA_VERSION}-1.tar.bz2"
+popd >/dev/null
